0f6dad5551bd680671f6505bf07cb7e7b73d8ac1
Suleman-Sehar <160259214+Suleman-Sehar@users.noreply.github.com>
2025-06-16 14:08:47 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -30,12 +30,27 @@ agent = Agent(
     instructions= "You are frontend specialist"
 )
 
+@cl.on_chat_start
+async def handle_start():
+    cl.user_session.set("history", [])
+    await cl.Message(
+        content = "Hello from Suleman Sehar. How can I help you?").send()
+
 @cl.on_message
 async def handle_message(message : cl.Message):
-    content = message.content
+    history = cl.user_session.get("history", [])
+    history.append({
+        "role": "user",
+        "content": message.content
+    })
     result = await Runner.run(
         agent,
-        input = message.content,
+        input = history,
         run_config = config
     )
+    history.append({
+        "role": "assistant",
+        "content": result.final_output
+    })
+    cl.user_session.set("history", history)
     await cl.Message(content = result.final_output).send()
